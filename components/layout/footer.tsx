0236c6aebbfb3e1d189760eb7ac5664aae08fc67
Yaqub-J <yaqubishaqjae@gmail.com>
2024-12-26 03:35:38 +0100

    footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -40,12 +40,12 @@ export default async function Footer() {
         <div className="md:ml-auto">
           <a
             className="flex h-8 w-max flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
-            aria-label="Deploy on Vercel"
-            href="https://vercel.com/templates/next.js/nextjs-commerce"
+            aria-label="Connect with us now."
+            href="https://wa.me/2347087681616"
           >
-            <span className="px-3">▲</span>
+            <span className="px-3">Call</span>
             <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
-            <span className="px-3">Deploy</span>
+            <span className="px-3">Chat</span>
           </a>
         </div>
       </div>
@@ -56,12 +56,9 @@ export default async function Footer() {
             {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
-          <p>
-            <a href="https://github.com/vercel/commerce">View the source</a>
-          </p>
           <p className="md:ml-auto">
-            <a href="https://vercel.com" className="text-black dark:text-white">
-              Created by ▲ Vercel
+            <a href="https://cyberaim.ng/" className="text-black dark:text-white">
+              By Cyber-aim
             </a>
           </p>
         </div>
